5d61871511962ed1e6f82832c9dff93d4179f167
ranjeetnaik <ranjeetnaik@gmail.com>
2022-04-04 21:54:42 +0300

    IBAN lookup and validation service first commit

---
+++ src/main/java/com/ibanlookup/ibanlookupvalidate/IbanlookupValidateApplication.java
@@ -0,0 +1,13 @@
+package com.ibanlookup.ibanlookupvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanlookupValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanlookupValidateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibanlookup/ibanlookupvalidate/IbanlookupValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibanlookup.ibanlookupvalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanlookupValidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
